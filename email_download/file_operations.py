fbc88c05fb95f3d92646a6054354d8c0def730a8
Christian Pojoni <dev@pojoni.at>
2023-10-12 14:12:43 +0200

    Check if save_directory contains only mails

--- email_download/file_operations.py
+++ email_download/file_operations.py
@@ -17,7 +17,14 @@ def clear_directory(directory: str):
 
 
 def manage_directory(directory: str, delete_contents: bool):
+    files = os.listdir(directory)
     if os.path.exists(directory) and os.listdir(directory):
+        only_emails = all(f.endswith('.eml') for f in files)
+
+        if not only_emails:
+            print(f"'{directory}' contains non-email files.")
+            return
+
         if delete_contents or click.confirm(f"'{directory}' is not empty. Do you want to delete its contents?",
                                             default=False):
             clear_directory(directory)
