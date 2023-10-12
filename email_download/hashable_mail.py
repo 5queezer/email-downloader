f2e094e4399720235bf3113624f710383ccd4287
Christian Pojoni <dev@pojoni.at>
2023-10-12 17:42:47 +0200

    Unsubscribe link

--- email_download/hashable_mail.py
+++ email_download/hashable_mail.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 import re
 from bs4 import BeautifulSoup
 from email import message_from_string
-
+from imap_tools.message import MailMessage
 
 class DictCompatible(ABC):
     @abstractmethod
@@ -19,6 +19,7 @@ class HashableEmailAddress(EmailAddress, DictCompatible):
     def __init__(self, value: EmailAddress):
         super().__init__(value.name, value.email)
         self.domain = value.email.split('@')[-1]
+        self.unsubscribe_link = None
 
     def __hash__(self):
         return hash(self.email)
@@ -34,40 +35,30 @@ def email_address_to_dict(a: HashableEmailAddress, count):
         "Name": a.name,
         "Email": a.email,
         "Domain": a.domain,
-        "Count": count
+        "Count": count,
+        "Unsubscribe": a.unsubscribe_link
     }
 
 
-def extract_unsubscribe_links(email_content: str):
+def extract_unsubscribe_links(email_msg: MailMessage) -> str | None:
+    def contains_unsubscribe_keyword(text):
+        return re.search("unsubscribe|abmelden", text, re.I)
 
-    # Parse the email content
-    msg = message_from_string(email_content)
+    # Extract from the plain text content
+    if hasattr(email_msg, 'text') and email_msg.text:
+        for line in email_msg.text.splitlines():
+            if contains_unsubscribe_keyword(line):
+                potential_links = re.findall(r'https?://\S+', line)
+                for link in potential_links:
+                    if contains_unsubscribe_keyword(link):
+                        return link
 
-    # Initialize an empty list to store unsubscribe links
-    unsubscribe_links = []
+    # Extract from the HTML content
+    if hasattr(email_msg, 'html') and email_msg.html:
+        soup = BeautifulSoup(email_msg.html, 'html.parser')
+        for link in soup.find_all('a', href=True):
+            if contains_unsubscribe_keyword(link['href']) or contains_unsubscribe_keyword(link.get_text()):
+                return link['href']
 
-    # Extract plain text and HTML bodies
-    if msg.is_multipart():
-        for part in msg.walk():
-            content_type = part.get_content_type()
-            content_disposition = str(part.get("Content-Disposition"))
-
-            # skip any text/plain (txt) attachments
-            if "attachment" in content_disposition:
-                continue
-            if content_type == "text/plain":
-                body = part.get_payload(decode=True).decode()
-                unsubscribe_links.extend(re.findall(r'https?://\S+', body))
-            elif content_type == "text/html":
-                body = part.get_payload(decode=True).decode()
-                soup = BeautifulSoup(body, 'html.parser')
-                for link in soup.find_all('a', href=True):
-                    if "unsubscribe" in link['href'].lower():
-                        unsubscribe_links.append(link['href'])
-    else:
-        # For non-multipart emails
-        body = msg.get_payload(decode=True).decode()
-        unsubscribe_links.extend(re.findall(r'https?://\S+', body))
-
-    # Return a list of unique unsubscribe links
-    return list(set(unsubscribe_links))
+    # If no unsubscribe links found, return None
+    return None
